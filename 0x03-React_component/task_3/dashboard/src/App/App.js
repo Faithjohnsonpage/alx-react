3a781ed0c1d7bae679f4baf3e5456d37dc47e415
Faithjohnsonpage <ermac_4life@yahoo.com>
2025-02-02 21:01:22 +0100

    Task 3

---
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import "./App.css";
+import Notifications from "../Notifications/Notifications";
+import Login from "../Login/Login";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import CourseList from "../CourseList/CourseList";
+import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
+
+class App extends Component {
+  handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === "h") {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+
+    const listCourses = [
+      { id: 1, name: "ES6", credit: 60 },
+      { id: 2, name: "Webpack", credit: 20 },
+      { id: 3, name: "React", credit: 40 },
+    ];
+
+    const listNotifications = [
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+      { id: 3, type: "default", html: { __html: getLatestNotification() } },
+    ];
+
+    return (
+      <>
+        <Notifications displayDrawer={true} listNotifications={listNotifications} />
+        <Header />
+        <hr />
+        {!isLoggedIn ? <Login /> : <CourseList listCourses={listCourses} />}
+        <hr />
+        <Footer />
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+
+export default App;
