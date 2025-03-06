9e6bf083681006c1ef058509ba2905fb1189a9d4
Faithjohnsonpage <ermac_4life@yahoo.com>
2025-03-06 11:51:48 +0100

    7-equality.js

---
+++ 0x06-React_Immutable/7-equality.js
@@ -0,0 +1,16 @@
+import { Map, is } from 'immutable';
+
+export function areMapsEqual (map1, map2) {
+  return is(map1, map2);
+}
+
+const map1 = Map({
+  firstName: 'Guillaume',
+  lastName: 'Salva'
+});
+const map2 = Map({
+  firstName: 'Guillaume',
+  lastName: 'Salva'
+});
+
+console.log(areMapsEqual(map1, map2));
